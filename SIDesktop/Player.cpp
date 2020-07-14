4877bac68e85f1c716780b05acfb7ddd8d9d99d8
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-07-14 19:30:19 +0300

    Use Rect ref in Player Update

--- SIDesktop/Player.cpp
+++ SIDesktop/Player.cpp
@@ -16,7 +16,7 @@ SI::Player::~Player()
 
 void SI::Player::Update()
 {
-	SDL_Rect dest = this->GetDestinationRect();
+	SDL_Rect& dest = this->GetDestinationRect();
 
 	switch (SI::Game::Event.type)
 	{
@@ -42,9 +42,4 @@ void SI::Player::Update()
 		default:
 			break;
 	}
-
-	dest.x = SIMath::Clamp(dest.x, 0, 800 - dest.w);
-	dest.y = SIMath::Clamp(dest.y, 0, 600 - dest.h);
-	
-	this->SetPos(dest.x, dest.y);
 }
